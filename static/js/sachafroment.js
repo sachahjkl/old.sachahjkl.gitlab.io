62357bdcf9be5c1cd56163771068914dc1c13103
SachaFroment <daftsacha@gmail.com>
2019-01-17 23:19:15 +0100

    dark mode 2

--- static/js/sachafroment.js
+++ static/js/sachafroment.js
@@ -3,7 +3,9 @@ document.body.className = "dark";
 function dark_mode() {
   if (document.body.className == "dark") {
     document.body.className = "";
+    document.getElementById("darkbtn").firstChild.data ="Dark Mode"
   } else {
     document.body.className = "dark";
+    document.getElementById("darkbtn").firstChild.data ="Light Mode"
   }
 }
