9b02e240167b9427c27a3964966deef888790266
Sacha FROMENT <sacha.froment@gmail.com>
2020-08-26 13:34:20 +0000

    cleanup

--- static/js/sachafroment.js
+++ static/js/sachafroment.js
@@ -1,11 +1,22 @@
-document.body.className = "dark";
+document.addEventListener("DOMContentLoaded", function(){
+	let mode = localStorage.getItem("mode");
+	console.log(mode)
+	if(mode == ""){
+		localStorage.setItem("mode","light");
+	} else if (mode == "dark"){
+		document.body.className = "dark";
+		document.getElementById("darkbtn").firstChild.data = "Light Mode";
+	}
+});
 
 function dark_mode() {
   if (document.body.className == "dark") {
-    document.body.className = "";
-    document.getElementById("darkbtn").firstChild.data ="Dark Mode"
-  } else {
-    document.body.className = "dark";
-    document.getElementById("darkbtn").firstChild.data ="Light Mode"
+	localStorage.setItem("mode","light");
+	document.body.className = "light";
+	document.getElementById("darkbtn").firstChild.data ="Dark Mode";
+  } else if (document.body.className == "light" || document.body) {
+	localStorage.setItem("mode","dark");
+	document.body.className = "dark";
+	document.getElementById("darkbtn").firstChild.data ="Light Mode";
   }
 }
